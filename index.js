38693b00b817c2417b90def9c30bc848922f70d4
Michael Chinn <mchinn614@gmail.com>
2019-01-22 18:27:20 -0800

    updated styling, added clear search

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function search(input){
       ])
       .then(results => {
         const response = results.filter(item=>item.ok).map(item=>item.json());
+        if (response.length===0){
+            console.log(response.length)
+            $('.drug-list').html(`<h3>No results found. Please search again.</h3>`)
+        }
         return Promise.all(response);
       })
       .then(responseJson=>{
@@ -60,6 +64,7 @@ function renderMultipleResults(resultsJson,numResults){
 //when company is chosen by user, then handle selection, use search function by ID
 function handleSelection(){
     $('#companyButton').on('click',function(){
+        $('.drug-list').empty();
         api.getFdaData('id',$(this).attr('class'),displayApiError)
         .then(response=>response.json())
         .then(responseJson=>renderData(responseJson))
@@ -86,13 +91,14 @@ function renderCompanyNews(companyName,newsJson){
     const articles = newsJson.articles;
     for (let i=0;i<articles.length;i++){
         $('.news').append(
-            `<a href=${articles[i].url} target='_blank'>
-                <ul>${articles[i].title}
+            `<a href=${articles[i].url} class='story' target='_blank'>
+                <span class='article-title'>${articles[i].title}</span>
                     <div class ='news-wrapper'>
-                        <li class='news-source'>${articles[i].source.name}</li>
-                        <li class-'news-date'>${Date(articles[i].publishedAt)}</li> 
+                        <ul>
+                            <li class='news-source'>${articles[i].source.name}</li>
+                            <li class-'news-date'>${Date(articles[i].publishedAt)}</li> 
+                        </ul>
                     </div>   
-                </ul>
             </a>`)
     }
     
@@ -108,8 +114,8 @@ function renderCompanyDrugList(companyName){
             $('.drugs').append(
                 `<button type='submit' class='drug-name'>
                     <h4>
-                    ${responseJson.results[i].openfda.brand_name} (
-                    ${responseJson.results[i].openfda.generic_name})
+                    ${responseJson.results[i].openfda.brand_name} 
+                    (${responseJson.results[i].openfda.generic_name})
                     </h4>
                     <p class='hide'>${responseJson.results[i].indications_and_usage}</p>
                 </button>`
@@ -148,14 +154,33 @@ function displayApiError(error){
     $('form').append(`<p class='error'>${error.message}</p>`)
 };
 
+//clear search
+function clearSearch(){
+    $('.clear-search').on('click',event=>{
+        event.preventDefault();
+        $('.news').empty();
+        $('.drugs').empty();
+        $('.drug-list').empty();
+        $('.instructions').removeClass('hide')
+        $('.search').val('').focus();
+        $('.clear-search').addClass('hide')
+    })
+}
+
 //callback function to start function
 function handleSubmit(){
     $('.search-form').on('submit',event=>{
         event.preventDefault();
         $('body').removeClass('center-body');
+        $('.clear-search').removeClass('hide');
+        $('.news').empty();
+        $('.drugs').empty();
+        $('.drug-list').empty();
+        $('.instructions').addClass('hide')
         const userInput=$('.search').val();
         search(userInput);
     });
 };
 
 $(handleSubmit);
+$(clearSearch);
