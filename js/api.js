1fef7c72be68d1a7bc6043bf1a2578bbffecd607
Michael Chinn <mchinn614@gmail.com>
2019-01-27 14:48:08 -0800

    styling changes made

--- js/api.js
+++ js/api.js
@@ -1,44 +1,46 @@
 // functions used to call OpenFDA API and News API
 'use strict';
 
-const api = (function () {
+const api = (function() {
+  const fdaUrl = 'https://api.fda.gov/drug/';
+  const newsUrl = 'https://newsapi.org/v2/';
 
-    const fdaUrl = "https://api.fda.gov/drug/";
-    const newsUrl = "https://newsapi.org/v2/"
+  function getFdaData(field, input, failureCallback, limit = 1) {
+    return fetch(
+      fdaUrl + 'label.json?search=' + utils.searchString(field, input) + `&limit=${limit}`
+    ).catch(error => failureCallback(error));
+  }
 
-    function getFdaData(field, input, failureCallback, limit=1) {
-
-        return (
-            
-            fetch(fdaUrl + 'label.json?search=' + utils.searchString(field,input)+ `&limit=${limit}`)
-            .catch(error=>failureCallback(error))
-
-        );
-
-    }
-
-    function paginateNews(companyName,dataContainer,pageContainer,loadingCallback,displayCallback){
-
-        const query = companyName + ' AND drug'
-        const url = `${newsUrl}everything?language=en&apiKey=8454a788c9ee43aaa925a9c288118ed7&q=
+  function paginateNews(
+    companyName,
+    dataContainer,
+    pageContainer,
+    loadingCallback,
+    displayCallback
+  ) {
+    const query = companyName + ' AND drug';
+    const url = `${newsUrl}everything?language=en&apiKey=8454a788c9ee43aaa925a9c288118ed7&q=
             ${query}&sortBy=popularity`;
-        const alias = {
-            pageNumber:'page',
-        }
+    const alias = {
+      pageNumber: 'page'
+    };
+    const pageSize = 10;
 
-        const pageSize = 10;
+    utils.paginate(
+      url,
+      'articles',
+      'totalResults',
+      pageContainer,
+      dataContainer,
+      pageSize,
+      alias,
+      loadingCallback,
+      displayCallback
+    );
+  }
 
-        utils.paginate(url,'articles','totalResults',pageContainer,dataContainer,pageSize,alias,loadingCallback,displayCallback);
-    
-    }
-
- 
-  return { 
-      
+  return {
     getFdaData,
     paginateNews
-
   };
-}());
-
-
+})();
--- js/index.js
+++ js/index.js
@@ -1,84 +1,100 @@
-'use strict'
+'use strict';
 
-//Searches user input against 3 OpenFDA endpoints. 
+//Searches user input against 3 OpenFDA endpoints.
 // If only one successful request, then render news and drug list for company
 //If more than one successful request then prioritize in order: manufacturer_name, brand_name, generic_name
 // and render list of companies for user to choose from
-function search(input){
-    $('section').empty();
-    Promise.all([
-        api.getFdaData("openfda.manufacturer_name",input,displayApiError),
-        api.getFdaData("openfda.brand_name",input,displayApiError),
-        api.getFdaData("openfda.generic_name",input,displayApiError)
-      ])
-      .then(results => {
-        const response = results.filter(item=>item.ok).map(item=>item.json());
-        if (response.length===0){
-            $('.drug-list').html(`<h3>No results found. Please search again.</h3>`)
+function search(input) {
+  $('section').empty();
+  Promise.all([
+    api.getFdaData('openfda.manufacturer_name', input, displayApiError),
+    api.getFdaData('openfda.brand_name', input, displayApiError),
+    api.getFdaData('openfda.generic_name', input, displayApiError)
+  ])
+    .then(results => {
+      const response = results.filter(item => item.ok).map(item => item.json());
+      if (response.length === 0) {
+        $('.drug-list').html(`<h3>No results found. Please search again.</h3>`);
+      }
+      return Promise.all(response);
+    })
+    .then(responseJson => {
+      const numResults = responseJson.map(item => item.meta.results.total);
+      for (let i = 0; i < numResults.length; i++) {
+        if (numResults[i] === 1) {
+          renderData(responseJson[i]);
+        } else if (numResults[i] > 1) {
+          renderMultipleResults(responseJson[i], numResults[i]);
+          break;
         }
-        return Promise.all(response);
-      })
-      .then(responseJson=>{
-        const numResults = responseJson.map(item=>item.meta.results.total);
-        for (let i=0;i<numResults.length;i++){
-            if (numResults[i]===1){
-                renderData(responseJson[i]);
-            }
-            else if (numResults[i]>1){
-                renderMultipleResults(responseJson[i],numResults[i])
-                break
-            }
-        }
-      })
-      .catch(error=>displayUnknownError(error))
+      }
+    })
+    .catch(error => displayUnknownError(error));
 }
 
-
 //if more than one result of search function is found, then render button for each unique company
-function renderMultipleResults(resultsJson,numResults){
-    const limit = (numResults<99) ? numResults:99;
-    api.getFdaData('openfda.manufacturer_name',resultsJson.results[0].openfda.manufacturer_name,limit)
-    .then(response=>response.json())
-    .then(responseJson=>{
-        $('.drug-list').append(`<h5>Please select company from list.</h5>`)
-        let companyList =[];
-        for (let i=0;i<responseJson.results.length;i++){
-            let companyName = responseJson.results[i].openfda.manufacturer_name[0];
-            let id = responseJson.results[i].id;
-            if (companyName.indexOf($('.search').val()>=0) && !companyList.includes(companyName.split(/[ ,.]/).join('').toLowerCase())){
-                companyList.push(companyName.split(/[ ,.]/).join('').toLowerCase());
-                $('.drug-list').append(
-                    `<button id='companyButton' class='${id}'>${companyName}</button>`);
-            }
+function renderMultipleResults(resultsJson, numResults) {
+  const limit = numResults < 99 ? numResults : 99;
+  api
+    .getFdaData(
+      'openfda.manufacturer_name',
+      resultsJson.results[0].openfda.manufacturer_name,
+      limit
+    )
+    .then(response => response.json())
+    .then(responseJson => {
+      $('.drug-list').append(`<h5>Please select company from list.</h5>`);
+      let companyList = [];
+      for (let i = 0; i < responseJson.results.length; i++) {
+        let companyName = responseJson.results[i].openfda.manufacturer_name[0];
+        let id = responseJson.results[i].id;
+        if (
+          companyName.indexOf($('.search').val() >= 0) &&
+          !companyList.includes(
+            companyName
+              .split(/[ ,.]/)
+              .join('')
+              .toLowerCase()
+          )
+        ) {
+          companyList.push(
+            companyName
+              .split(/[ ,.]/)
+              .join('')
+              .toLowerCase()
+          );
+          $('.drug-list').append(
+            `<button id='companyButton' class='${id}'>${companyName}</button>`
+          );
         }
-    
-        $(handleSelection);
+      }
+
+      $(handleSelection);
     })
-    .catch(error=>displayUnknownError(error))
-
-};
-
+    .catch(error => displayUnknownError(error));
+}
 
 //renders news and and drug data view
-function renderData(json){
-    $('.clear-search').removeClass('hide');
-    const companyName = json.results[0].openfda.manufacturer_name;
-    $('.news').before(`<section role='region' class='news-header'><h3>News About ${companyName}</h3></section>`);
-    api.paginateNews(companyName,'.news','.page-news',loading,renderNewsPages);
-    renderCompanyDrugList(companyName);
+function renderData(json) {
+  $('.clear-search').removeClass('hide');
+  const companyName = json.results[0].openfda.manufacturer_name;
+  $('.news').before(
+    `<section role='region' class='news-header'><h3>News About ${companyName}</h3></section>`
+  );
+  api.paginateNews(companyName, '.news', '.page-news', loading, renderNewsPages);
+  renderCompanyDrugList(companyName);
 }
 
 //callback function while pagination is loading
-function loading(){
-    $('.news').html('Loading data...')
-};
-
+function loading() {
+  $('.news').html('Loading data...');
+}
 
 // creates div for each result from news API request for pagination
-function renderNewsPages(articles){
-
-    return (
-        articles.map(item=>
+function renderNewsPages(articles) {
+  return articles
+    .map(
+      item =>
         `<div><a href=${item.url} class='story' target='_blank'>
         <span class='article-title'>${item.title}</span>
             <div class ='news-wrapper'>
@@ -88,100 +104,106 @@ function renderNewsPages(articles){
                 </ul>
             </div>   
         </a></div>`
-        ).join('')
-
     )
-
-};
-
+    .join('');
+}
 
 //renders list of drugs made by company and allows user to view indications of each medicine
-function renderCompanyDrugList(companyName){
-    api.getFdaData('openfda.manufacturer_name',companyName,displayApiError,10)
-    .then(response=>response.json())
-    .then(responseJson=>{
-        $('.drugs').before(`<section role='section' class='drugs-header'><h3>Medicines made by ${companyName}</h3></section>`)
-        for (let i=0;i<responseJson.results.length;i++){
-            $('.drugs').append(
-                `<button type='submit' class='drug-name'>
+function renderCompanyDrugList(companyName) {
+  api
+    .getFdaData('openfda.manufacturer_name', companyName, displayApiError, 10)
+    .then(response => response.json())
+    .then(responseJson => {
+      $('.drugs').before(
+        `<section role='section' class='drugs-header'><h3>Medicines made by ${companyName}</h3></section>`
+      );
+      for (let i = 0; i < responseJson.results.length; i++) {
+        $('.drugs').append(
+          `<button type='submit' class='drug-name'>
                     <h4>
                     ${responseJson.results[i].openfda.brand_name} 
                     (${responseJson.results[i].openfda.generic_name})
                     </h4>
                     <p class='hide'>${responseJson.results[i].indications_and_usage}</p>
                 </button>`
-            )
-        }
-        $(toggleDrugDescription);
+        );
+      }
+      $(toggleDrugDescription);
     })
-    .catch(error=>displayError(error))
-};
+    .catch(error => displayError(error));
+}
 
 //when company is chosen by user, then handle selection, use search function by ID
-function handleSelection(){
-    $('#companyButton').on('click',function(){
-        $('.drug-list').empty();
-        api.getFdaData('id',$(this).attr('class'),displayApiError)
-        .then(response=>response.json())
-        .then(responseJson=>renderData(responseJson))
-        .catch(error=>displayUnknownError(error));
-
-        })
-
-};
+function handleSelection() {
+  $('#companyButton').on('click', function() {
+    $('.drug-list').empty();
+    api
+      .getFdaData('id', $(this).attr('class'), displayApiError)
+      .then(response => response.json())
+      .then(responseJson => renderData(responseJson))
+      .catch(error => displayUnknownError(error));
+  });
+}
 
 //allows user to show and hide indications of drugs
-function toggleDrugDescription(){
-    $('.drug-name').on('click',function(){
-        const display = $(this).find("p").attr('class');
-        if (display==='hide') {
-            $(this).find("p").removeClass('hide');
-            $(this).addClass('show')
-        } 
-        else{
-            $(this).removeClass('show');
-            $(this).find("p").addClass('hide');
-        }
-    })
-};
+function toggleDrugDescription() {
+  $('.drug-name').on('click', function() {
+    const display = $(this)
+      .find('p')
+      .attr('class');
+    if (display === 'hide') {
+      $(this)
+        .find('p')
+        .removeClass('hide');
+      $(this).addClass('show');
+    } else {
+      $(this).removeClass('show');
+      $(this)
+        .find('p')
+        .addClass('hide');
+    }
+  });
+}
 
 //error handling for generic errors
-function displayUnknownError(error){
-    $('section').each(section=>$('section').empty());
-    $('.drug-list').append(`<h3 class='error'>Unknown Error</h3>`)
+function displayUnknownError(error) {
+  $('section').each(section => $('section').empty());
+  $('.drug-list').append(`<h3 class='error'>Unknown Error</h3>`);
 }
 
 //error handling for API request error
-function displayApiError(error){
-    $('section').each(section=>$('section').empty());
-    $('.drug-list').append(`<h3 class='error'>${error.message}</h3>`)
-};
+function displayApiError(error) {
+  $('section').each(section => $('section').empty());
+  $('.drug-list').append(`<h3 class='error'>${error.message}</h3>`);
+}
 
 //clear search button functioality
-function clearSearch(){
-    $('.clear-search').on('click',event=>{
-        event.preventDefault();
-        $('section').empty();
-        $('.instructions').removeClass('hide')
-        $('.search').val('').focus();
-        $('.clear-search').addClass('hide')
-    })
+function clearSearch() {
+  $('.clear-search').on('click', event => {
+    event.preventDefault();
+    $('section').empty();
+    $('.instructions').removeClass('hide');
+    $('.search')
+      .val('')
+      .focus();
+    $('.clear-search').addClass('hide');
+  });
 }
 
 //callback function to start function
-function handleSubmit(){
-    $('.search-form').on('submit',event=>{
-        event.preventDefault();
-        $('body').removeClass('center-body');
-        $('section').empty();
-        $('.drugs-header').remove();
-        $('.news-header').remove();
-        $('.clear-search').addClass('hide')
-        $('.instructions').addClass('hide')
-        const userInput=$('.search').val();
-        search(userInput);
-    });
-};
+function handleSubmit() {
+  $('.search-form').on('submit', event => {
+    event.preventDefault();
+    $('body').removeClass('center-body');
+    $('section').empty();
+    $('.drugs-header').remove();
+    $('.news-header').remove();
+    $('.clear-search').addClass('hide');
+    $('.instructions').addClass('hide');
+    const userInput = $('.search').val();
+    search(userInput);
+  });
+}
 
 $(handleSubmit);
 $(clearSearch);
